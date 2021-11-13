0f8219c1b8aa89358d86269acc2327d55c818ea4
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-13 19:55:40 +1030

    Contact component markup

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import Footer from '../footer/footer';
 
 class Contact extends Component {
   render() {
-    return <div>Contact Us !!</div>;
+    return (
+      <>
+        <div>
+          <h3>Let's have a talk together!</h3>
+        </div>
+        <div>
+          <h2>Contact Us</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+            ducimus dolor praesentium tempora mollitia voluptatibus atque
+            consectetur perspiciatis. Quis voluptatum commodi earum qui ipsum
+            consequuntur exercitationem cum quam quasi inventore.
+          </p>
+        </div>
+        <div>
+          <div>
+            <label for="name">Your name (required)</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label for="email">Your Email (required)</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label for="message">Your Message</label>
+            <input type="text" />
+          </div>
+          <button>SUBMIT</button>
+        </div>
+        <Footer />
+      </>
+    );
   }
 }
 

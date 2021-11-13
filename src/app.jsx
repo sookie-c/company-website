f3b9829bee0fbe1012ab75999e9f03b86bbd96af
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-13 11:39:27 +1030

    Home component markup

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,14 @@
 import './app.css';
 import Navbar from './components/navbar/navbar';
+import Home from './components/home/home';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/home/home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <h5>Technology Specialist</h5>
+        <h1>Creating a One Stop Shop for your Telco and IT needs</h1>
+        <h4>
+          Creating a One Stop SHop for your Telco and IT needs One Solution is
+          your Premium Telstra Enterprise and Business Partner, and specialises
+          in designing solutions to suit your business objectives
+        </h4>
+        <button>SERVICES</button>
+      </>
+    );
+  }
+}
+
+export default Home;

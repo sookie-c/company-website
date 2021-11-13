fd792734f04f899274195cc322ef839cd84806fc
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-13 22:05:38 +1030

    CSS navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,36 +2,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStreetView } from '@fortawesome/free-solid-svg-icons';
+import styles from './navbar.module.css';
 
 class Navbar extends Component {
   render() {
     return (
-      <>
-        <div className="logo">
-          <Link to="/">
-            {' '}
+      <header className={styles.navbar}>
+        <div>
+          <Link to="/" className={styles.logo}>
             <FontAwesomeIcon icon={faStreetView} />
-            <span>Company</span>
+            <span className={styles.company}>Company</span>
           </Link>
         </div>
-        <ul>
+        <ul className={styles.menu}>
           <Link to="/">
             <li>Home</li>
           </Link>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/solution">Solution</Link>
-          </li>
-          <li>
-            <Link to="/careers">Careers</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/solution">
+            <li>Solution</li>
+          </Link>
+          <Link to="/careers">
+            <li>Careers</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact Us</li>
+          </Link>
         </ul>
-      </>
+      </header>
     );
   }
 }

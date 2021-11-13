f6db5c8d20c3efec1802cd8fdf82604482554508
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-14 01:29:36 +1030

    CSS home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -10,102 +10,138 @@ import {
   faTrophy,
 } from '@fortawesome/free-solid-svg-icons';
 import Footer from '../footer/footer';
+import styles from './home.module.css';
 
 class Home extends Component {
   render() {
     return (
       <>
-        <div className="main">
-          <h5>Technology Specialist</h5>
-          <h1>Creating a One Stop Shop for your Telco and IT needs</h1>
-          <h4>
+        <div className={styles.main}>
+          <h5 className={styles.tinyTitle}>Technology Specialist</h5>
+          <h1 className={styles.mainTitle}>
+            Creating a One Stop Shop for your Telco and IT needs
+          </h1>
+          <h4 className={styles.mainDescription}>
             Creating a One Stop SHop for your Telco and IT needs One Solution is
             your Premium Telstra Enterprise and Business Partner, and
             specialises in designing solutions to suit your business objectives
           </h4>
-          <button>SERVICES</button>
+          <button className={styles.serviceBtn}>Services</button>
         </div>
-        <div className="description">
-          <img src="img/laptop.jpg" alt="description image" />
-          <div>
+        <div className={styles.description}>
+          <img
+            className={styles.descriptionImg}
+            src="img/laptop.jpg"
+            alt="description image"
+          />
+          <div className={styles.text}>
             <h2>The best way to work together on a task</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
               ea vitae odio, repudiandae a suscipit odit cumque totam saepe,
               dolorum quis, placeat consequatur voluptas. Ratione veritatis
-              exercitationem dolor labore rem.
+              exercitationem dolor labore rem. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Veritatis, aspernatur rerum,
+              laboriosam eos veniam repellendus vitae delectus, facilis earum
+              natus reiciendis repudiandae ea! Nostrum et ducimus sapiente
+              pariatur aliquam odio!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at,
+              atque consequatur facilis nam labore ab quidem quasi quas fugit,
+              harum nulla eos voluptas excepturi delectus omnis! Obcaecati,
+              animi quo.
             </p>
           </div>
         </div>
-        <div className="specialisation">
-          <h2>We Specialise In</h2>
-          <ul>
-            <li>
+        <div className={styles.specialisation}>
+          <h1 className={styles.specialTitle}>We Specialise In</h1>
+          <div className={styles.specialItems}>
+            <div className={styles.specialItem}>
               <img
+                className={styles.specialIcon}
                 src="img/specialisation/business-solution.png"
                 alt="business solution"
               />
-              <span>Business Solution</span>
-            </li>
-            <li>
+              <div>Business Solution</div>
+            </div>
+            <div className={styles.specialItem}>
               <img
+                className={styles.specialIcon}
                 src="img/specialisation/mobile-system.png"
                 alt="mobile system"
               />
-              <span>Phone & Mobile System</span>
-            </li>
-            <li>
-              <img src="img/specialisation/it-service.png" alt="it service" />
-              <span>Monaged IT Services</span>
-            </li>
-            <li>
+              <div>Phone & Mobile System</div>
+            </div>
+            <div className={styles.specialItem}>
               <img
+                className={styles.specialIcon}
+                src="img/specialisation/it-service.png"
+                alt="it service"
+              />
+              <div>Monaged IT Services</div>
+            </div>
+            <div className={styles.specialItem}>
+              <img
+                className={styles.specialIcon}
                 src="img/specialisation/telecommunication.png"
                 alt="telecommunication"
               />
-              <span>Telecommunication</span>
-            </li>
-          </ul>
+              <div>Telecommunication</div>
+            </div>
+          </div>
         </div>
-        <div className="services">
-          <div className="service">
-            <FontAwesomeIcon icon={faDesktop} />
+        <div className={styles.services}>
+          <div className={styles.service}>
+            <FontAwesomeIcon className={styles.serviceIcon} icon={faDesktop} />
             <h3>Server Administration</h3>
             <p>Managing and monitoring the performance on a file systems</p>
           </div>
-          <div className="service">
-            <FontAwesomeIcon icon={faNetworkWired} />
+          <div className={styles.service}>
+            <FontAwesomeIcon
+              className={styles.serviceIcon}
+              icon={faNetworkWired}
+            />
             <h3>Network Services</h3>
             <p>
               Provided by dedicated LAN administration personnel either onsite
               or remotely
             </p>
           </div>
-          <div className="service">
-            <FontAwesomeIcon icon={faLaptopMedical} />
+          <div className={styles.service}>
+            <FontAwesomeIcon
+              className={styles.serviceIcon}
+              icon={faLaptopMedical}
+            />
             <h3>Disaster Recovery</h3>
             <p>
               Business continuity strategy is the development of a detailed
               Disaster Recovery Plan
             </p>
           </div>
-          <div className="service">
-            <FontAwesomeIcon icon={faTools} />
+          <div className={styles.service}>
+            <FontAwesomeIcon className={styles.serviceIcon} icon={faTools} />
             <h3>Network Maintenance</h3>
             <p>Manage the performance and availability the LAN and WAN</p>
           </div>
-          <div className="service">
-            <FontAwesomeIcon icon={faShieldVirus} />
+          <div className={styles.service}>
+            <FontAwesomeIcon
+              className={styles.serviceIcon}
+              icon={faShieldVirus}
+            />
             <h3>Virus Support</h3>
             <p>Experts who can work with you to diagnose and remove viruses</p>
           </div>
-          <div className="service">
-            <FontAwesomeIcon icon={faFolderOpen} />
+          <div className={styles.service}>
+            <FontAwesomeIcon
+              className={styles.serviceIcon}
+              icon={faFolderOpen}
+            />
             <h3>Transition Services</h3>
             <p>Manage the seamless crossover and control for IT services</p>
           </div>
         </div>
-        <div className="information">
+        <div className={styles.information}>
           <h3>NEED MORE INFORMATION</h3>
           <h2>Contact us to enquire about your IT solution today</h2>
           <p>
@@ -114,30 +150,35 @@ class Home extends Component {
             omnis at accusamus, adipisci consequuntur nesciunt eius nostrum
             iusto incidunt. Eos, impedit!
           </p>
-          <button>CONTACT US</button>
+          <button className={styles.contactBtn}>Contact Us</button>
         </div>
-        <div className="news">
-          <h1>News from Our Company</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            laboriosam magni quae sunt exercitationem deserunt consequuntur
-            optio, iure sequi delectus ad vero quo eveniet mollitia nesciunt est
-            nulla molestias magnam?
-          </p>
-          <div className="quantifiable">
-            <FontAwesomeIcon icon={faTrophy} />
-            <h2>39%</h2>
-            <h3>Increase in sales</h3>
+        <div className={styles.news}>
+          <div className={styles.newsDescription}>
+            <h1>News from Our Company</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem laboriosam magni quae sunt exercitationem deserunt
+              consequuntur optio, iure sequi delectus ad vero quo eveniet
+              mollitia nesciunt est nulla molestias magnam?
+            </p>
           </div>
-          <div className="quantifiable">
-            <FontAwesomeIcon icon={faTrophy} />
-            <h2>99%</h2>
-            <h3>Customer satisfaction</h3>
-          </div>
-          <div className="quantifiable">
-            <FontAwesomeIcon icon={faTrophy} />
-            <h2>10,000</h2>
-            <h3>Customers</h3>
+
+          <div className={styles.newsContainer}>
+            <div className={styles.quantifiable}>
+              <FontAwesomeIcon className={styles.newsIcon} icon={faTrophy} />
+              <h2>39%</h2>
+              <h3>Increase in sales</h3>
+            </div>
+            <div className={styles.quantifiable}>
+              <FontAwesomeIcon className={styles.newsIcon} icon={faTrophy} />
+              <h2>99%</h2>
+              <h3>Customer satisfaction</h3>
+            </div>
+            <div className={styles.quantifiable}>
+              <FontAwesomeIcon className={styles.newsIcon} icon={faTrophy} />
+              <h2>10,000</h2>
+              <h3>Customers</h3>
+            </div>
           </div>
         </div>
         <Footer />

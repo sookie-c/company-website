15521397975cab3238c153e752389a584315f567
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-13 18:56:38 +1030

    About markup

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,72 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 class About extends Component {
   render() {
-    return <div>About !!</div>;
+    return (
+      <>
+        <header>
+          <h1>About</h1>
+        </header>
+        <section>
+          <div>
+            We pursue relationships based on transparency, persistence, mutual
+            trust, and integrity with our employees, customers and other
+            business partners.
+          </div>
+        </section>
+        <section>
+          <h2>Our team</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi,
+            magnam accusamus reiciendis voluptatibus ipsa dolores iste esse nam
+            fugit nemo vitae odio quae quasi facere repudiandae sed. Sint,
+            autem.
+          </p>
+          <div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserCircle} />
+              <p>Full Name</p>
+            </div>
+          </div>
+        </section>
+        <div>
+          <h3>
+            We're looking for smart and passionate people to join our team.
+          </h3>
+          <button>APPLY NOW</button>
+        </div>
+      </>
+    );
   }
 }
 

b19ba03de23ed5d2b2569f114146abbc66aec820
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-14 22:20:46 +1030

    responsive navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,7 +5,26 @@ import { faStreetView, faBars } from '@fortawesome/free-solid-svg-icons';
 import styles from './navbar.module.css';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    };
+  }
+
+  handleToggle = (e) => {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded,
+    });
+  };
+
+  closeMenu = () => {
+    this.setState({ isExpanded: false });
+  };
+
   render() {
+    const { isExpanded } = this.state;
     return (
       <nav className={styles.navbar}>
         <div className={styles.navLogo}>
@@ -14,25 +33,39 @@ class Navbar extends Component {
             <span className={styles.company}>Company</span>
           </Link>
         </div>
-        <ul className={styles.menu}>
+        <ul className={isExpanded ? styles.menuColumn : styles.menu}>
           <Link to="/">
-            <li className={styles.menuItem}>Home</li>
+            <li className={styles.menuItem} onClick={this.closeMenu}>
+              Home
+            </li>
           </Link>
           <Link to="/about">
-            <li className={styles.menuItem}>About</li>
+            <li className={styles.menuItem} onClick={this.closeMenu}>
+              About
+            </li>
           </Link>
           <Link to="/solution">
-            <li className={styles.menuItem}>Solution</li>
+            <li className={styles.menuItem} onClick={this.closeMenu}>
+              Solution
+            </li>
           </Link>
           <Link to="/careers">
-            <li className={styles.menuItem}>Careers</li>
+            <li className={styles.menuItem} onClick={this.closeMenu}>
+              Careers
+            </li>
           </Link>
           <Link to="/contact">
-            <li className={styles.menuItem}>Contact Us</li>
+            <li className={styles.menuItem} onClick={this.closeMenu}>
+              Contact Us
+            </li>
           </Link>
         </ul>
 
-        <FontAwesomeIcon className={styles.navToggle} icon={faBars} />
+        <FontAwesomeIcon
+          className={styles.navToggle}
+          icon={faBars}
+          onClick={this.handleToggle}
+        />
       </nav>
     );
   }

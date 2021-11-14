7998ef64fd3a285b8b1ff902c3da75b078ce4233
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-14 17:22:21 +1030

    CSS careers

--- src/components/careers/careers.jsx
+++ src/components/careers/careers.jsx
@@ -2,86 +2,156 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapPin } from '@fortawesome/free-solid-svg-icons';
 import Footer from '../footer/footer';
+import styles from './careers.module.css';
 
 class Careers extends Component {
   render() {
     return (
-      <>
-        <div>
-          <h1>Careers</h1>
-          <h4>
-            Dolor quaerat non ad? Mollitia, quis eligendi quasi nam, consectetur
-            dolore temporibus, nisi exercitationem iure necessitatibus accusamus
-            harum quam et.
-          </h4>
-        </div>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nam
-            assumenda totam perspiciatis, rem optio repellendus quas, quasi fuga
-            dolore, harum nobis enim eligendi. Animi magnam dicta sunt dolores
-            voluptates.
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
-            beatae quis minus amet id sit expedita molestias ipsum suscipit?
-          </p>
-          <button>APPLY NOW</button>
-        </div>
-        <div>
-          <img src="img/city.jpg" alt="city" />
-        </div>
-        <div>
-          <h2>Culture at Work</h2>
+      <div className={styles.careers}>
+        <div className={styles.career}>
+          <div>
+            <h1 className={styles.careersTitle}>Careers</h1>
+            <h4>
+              Dolor quaerat non ad? Mollitia, quis eligendi quasi nam,
+              consectetur dolore temporibus, nisi exercitationem iure
+              necessitatibus accusamus harum quam et.
+            </h4>
+          </div>
           <div>
             <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad nam
+              assumenda totam perspiciatis, rem optio repellendus quas, quasi
+              fuga dolore, harum nobis enim eligendi. Animi magnam dicta sunt
+              dolores voluptates.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
+              beatae quis minus amet id sit expedita molestias ipsum suscipit?
+            </p>
+            <button className={styles.applyBtn}>APPLY NOW</button>
+          </div>
+        </div>
+        <div className={styles.cityBackground}></div>
+        <div className={styles.culture}>
+          <h2 className={styles.cultureTitle}>Culture at Company</h2>
+          <div className={styles.cultureContainer}>
+            <p className={styles.cultureText}>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
               dignissimos laudantium. Porro odio consequuntur quibusdam corrupti
               reprehenderit reiciendis, quae commodi impedit ut ipsum molestiae
               mollitia necessitatibus, quisquam adipisci labore nulla!
             </p>
-            <img src="img/careers/work-culture1.jpg" alt="culture1" />
+            <img
+              className={styles.cultureImg}
+              src="img/careers/work-culture1.jpg"
+              alt="culture1"
+            />
           </div>
-          <div>
-            <p>
+          <div className={styles.cultureContainer}>
+            <p className={styles.cultureText}>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
               dignissimos laudantium. Porro odio consequuntur quibusdam corrupti
               reprehenderit reiciendis, quae commodi impedit ut ipsum molestiae
               mollitia necessitatibus, quisquam adipisci labore nulla!
             </p>
-            <img src="img/careers/work-culture2.jpg" alt="culture2" />
+            <img
+              className={styles.cultureImg}
+              src="img/careers/work-culture2.jpg"
+              alt="culture2"
+            />
           </div>
-          <div>
-            <h3>Office Locations</h3>
-            <div>
-              <FontAwesomeIcon icon={faMapPin} />
-              <span>Los Angeles, The United States</span>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faMapPin} />
-              <span>San Francisco, The United States</span>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faMapPin} />
-              <span>Seoul, South Korea</span>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faMapPin} />
-              <span>Sao Paulo, Brazil</span>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faMapPin} />
-              <span>Mumbai, India</span>
-            </div>
-            <div>
-              <FontAwesomeIcon icon={faMapPin} />
-              <span>Tokyo, Japan</span>
+          <div className={styles.locations}>
+            <h2 className={styles.locationTitle}>Office Locations</h2>
+            <div className={styles.locationContainer}>
+              <div className={styles.location}>
+                <img
+                  className={styles.locationImg}
+                  src="img/location.jpg"
+                  alt="location"
+                />
+                <div className={styles.locationName}>
+                  <FontAwesomeIcon
+                    className={styles.locationIcon}
+                    icon={faMapPin}
+                  />
+                  <span>Los Angeles, USA</span>
+                </div>
+              </div>
+              <div className={styles.location}>
+                <img
+                  className={styles.locationImg}
+                  src="img/location.jpg"
+                  alt="location"
+                />
+                <div className={styles.locationName}>
+                  <FontAwesomeIcon
+                    className={styles.locationIcon}
+                    icon={faMapPin}
+                  />
+                  <span>San Francisco, USA</span>
+                </div>
+              </div>
+              <div className={styles.location}>
+                <img
+                  className={styles.locationImg}
+                  src="img/location.jpg"
+                  alt="location"
+                />
+                <div className={styles.locationName}>
+                  <FontAwesomeIcon
+                    className={styles.locationIcon}
+                    icon={faMapPin}
+                  />
+                  <span>Seoul, South Korea</span>
+                </div>
+              </div>
+              <div className={styles.location}>
+                <img
+                  className={styles.locationImg}
+                  src="img/location.jpg"
+                  alt="location"
+                />
+                <div className={styles.locationName}>
+                  <FontAwesomeIcon
+                    className={styles.locationIcon}
+                    icon={faMapPin}
+                  />
+                  <span>Sao Paulo, Brazil</span>
+                </div>
+              </div>
+              <div className={styles.location}>
+                <img
+                  className={styles.locationImg}
+                  src="img/location.jpg"
+                  alt="location"
+                />
+                <div className={styles.locationName}>
+                  <FontAwesomeIcon
+                    className={styles.locationIcon}
+                    icon={faMapPin}
+                  />
+                  <span>Mumbai, India</span>
+                </div>
+              </div>
+              <div className={styles.location}>
+                <img
+                  className={styles.locationImg}
+                  src="img/location.jpg"
+                  alt="location"
+                />
+                <div className={styles.locationName}>
+                  <FontAwesomeIcon
+                    className={styles.locationIcon}
+                    icon={faMapPin}
+                  />
+                  <span>Tokyo, Japan</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-
         <Footer />
-      </>
+      </div>
     );
   }
 }

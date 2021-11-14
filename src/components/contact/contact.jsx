2d331f9eeab9b00f90928b236487b4f8ce7ef329
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-14 18:31:02 +1030

    CSS contact

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,39 +1,45 @@
 import React, { Component } from 'react';
 import Footer from '../footer/footer';
+import styles from './contact.module.css';
 
 class Contact extends Component {
   render() {
     return (
-      <>
+      <div className={styles.contact}>
+        <h2 className={styles.contactTalk}>Let's have a talk together!</h2>
         <div>
-          <h3>Let's have a talk together!</h3>
-        </div>
-        <div>
-          <h2>Contact Us</h2>
-          <p>
+          <h1 className={styles.contactTitle}>Contact Us</h1>
+          <p className={styles.contactText}>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
             ducimus dolor praesentium tempora mollitia voluptatibus atque
             consectetur perspiciatis. Quis voluptatum commodi earum qui ipsum
             consequuntur exercitationem cum quam quasi inventore.
           </p>
         </div>
-        <div>
-          <div>
-            <label for="name">Your name (required)</label>
-            <input type="text" />
+        <form className={styles.contactForm}>
+          <div className={styles.form}>
+            <label className={styles.label} for="name">
+              Your name (required):{' '}
+            </label>
+            <input className={styles.input} type="text" />
           </div>
-          <div>
-            <label for="email">Your Email (required)</label>
-            <input type="text" />
+          <div className={styles.form}>
+            <label className={styles.label} for="email">
+              Your Email (required):{' '}
+            </label>
+            <input className={styles.input} type="text" />
           </div>
-          <div>
-            <label for="message">Your Message</label>
-            <input type="text" />
+          <div className={styles.form}>
+            <label className={styles.label} for="message">
+              Your Message:{' '}
+            </label>
+            <input className={styles.messageInput} type="text" />
           </div>
-          <button>SUBMIT</button>
-        </div>
+          <input className={styles.submitBtn} type="submit" value="submit" />
+        </form>
+
         <Footer />
-      </>
+      </div>
     );
   }
 }

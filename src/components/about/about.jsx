7c768f2e681dda1d7d87d793b7f06533ba2d3e86
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-14 15:10:54 +1030

    CSS about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,72 +2,95 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import Footer from '../footer/footer';
+import styles from './about.module.css';
 
 class About extends Component {
   render() {
     return (
-      <>
-        <header>
-          <h1>About</h1>
-        </header>
-        <section>
-          <div>
+      <div>
+        <header className={styles.about}>
+          <h1 className={styles.aboutTitle}>About</h1>
+          <div className={styles.aboutText}>
             We pursue relationships based on transparency, persistence, mutual
             trust, and integrity with our employees, customers and other
             business partners.
           </div>
-        </section>
-        <section>
-          <h2>Our team</h2>
+        </header>
+        <section className={styles.team}>
+          <h1 className={styles.teamTitle}>Our team</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil nisi,
             magnam accusamus reiciendis voluptatibus ipsa dolores iste esse nam
             fugit nemo vitae odio quae quasi facere repudiandae sed. Sint,
             autem.
           </p>
-          <div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+          <div className={styles.people}>
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faUserCircle} />
+            <div className={styles.profile}>
+              <FontAwesomeIcon
+                className={styles.profileIcon}
+                icon={faUserCircle}
+              />
               <p>Full Name</p>
             </div>
           </div>
         </section>
-        <div>
+        <div className={styles.hiring}>
           <h3>
             We're looking for smart and passionate people to join our team.
           </h3>
-          <button>APPLY NOW</button>
+          <button className={styles.hiringBtn}>APPLY NOW</button>
         </div>
         <Footer />
-      </>
+      </div>
     );
   }
 }

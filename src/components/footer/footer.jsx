3d9a0a0fadee3e3370fa7824af42d869c9fae845
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-11-15 11:28:11 +1030

    Add up button on footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import styles from './footer.module.css';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends Component {
+  backToTop = () => {
+    window.scrollTo(0, 0);
+  };
   render() {
     return (
       <footer className={styles.footer}>
@@ -12,9 +16,15 @@ class Footer extends Component {
         <div>
           <FontAwesomeIcon className={styles.gitIcon} icon={faGithub} />
         </div>
+
         <p className={styles.copyrights}>
           © 2021 Sookyoung Cho - All rights reserved
         </p>
+        <FontAwesomeIcon
+          className={styles.upBtn}
+          icon={faArrowUp}
+          onClick={this.backToTop}
+        />
       </footer>
     );
   }
